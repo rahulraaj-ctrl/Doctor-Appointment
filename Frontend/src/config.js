8f6b51cf6528507af4ddae39c7478b7a4e819f4d
Rahul Raj <rajsingh958070@gmail.com>
2026-04-02 17:58:42 +0530

    some changes

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,4 +1,4 @@
 // API Configuration
-const API_URL = import.meta.env.VITE_API_URL || 'https://doctor-appointment-oc3s.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'https://doctor-appointment-7h98.onrender.com';
 
 export default API_URL;
